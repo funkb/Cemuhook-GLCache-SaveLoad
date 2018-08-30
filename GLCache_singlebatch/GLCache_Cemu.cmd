c7b0f4e5691c332a31c4daff8c6e566502831058
funkb <texasfunk101@gmail.com>
2018-08-30 12:09:16 -0500

    Update GLCache_Cemu.cmd

--- GLCache_singlebatch/GLCache_Cemu.cmd
+++ GLCache_singlebatch/GLCache_Cemu.cmd
@@ -1,10 +1,10 @@
 @echo off
-robocopy %cd%/GLCache C:/Users/%USERNAME%/AppData/Local/NVIDIA/GLCache /e /is /it
+robocopy ./GLCache C:/Users/%USERNAME%/AppData/Local/NVIDIA/GLCache /e /is /it
 cls
 @echo ==================================================================================================
 @echo DO NOT CLOSE ME, IF YOU CLOSE ME YOU WILL HAVE TO MANUALLY SAVE YOUR GLCACHE AFTER YOU CLOSE CEMU. 
 @echo ==================================================================================================
 @echo THIS PROGRAM WILL AUTO-SAVE YOUR GLCACHE ONCE CEMU CLOSES.
-start /w %cd%/Cemu.exe
-robocopy C:/Users/%USERNAME%/AppData/Local/NVIDIA/GLCache %cd%/GLCache /e /is /it
+start /w ./Cemu.exe
+robocopy C:/Users/%USERNAME%/AppData/Local/NVIDIA/GLCache ./GLCache /e /is /it
 
